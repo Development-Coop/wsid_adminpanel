69964243edcd5884b14d57eac3a57ffd98a708ac
vigneshwaran k <kvigneshwaran60@gmail.com>
2025-01-06 15:47:44 +0530

    resolve-router-issue

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { route } from 'quasar/wrappers'
-import { createRouter, createMemoryHistory, createWebHistory, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import routes from './routes'
 import setupBeforeEach from "./beforeEach";
 /*
@@ -12,9 +12,6 @@ import setupBeforeEach from "./beforeEach";
  */
 
 export default route(function (/* { store, ssrContext } */) {
-  const createHistory = process.env.SERVER
-    ? createMemoryHistory
-    : (process.env.VUE_ROUTER_MODE === 'history' ? createWebHistory : createWebHashHistory)
 
   const Router = createRouter({
     scrollBehavior: () => ({ left: 0, top: 0 }),
@@ -23,7 +20,7 @@ export default route(function (/* { store, ssrContext } */) {
     // Leave this as is and make changes in quasar.conf.js instead!
     // quasar.conf.js -> build -> vueRouterMode
     // quasar.conf.js -> build -> publicPath
-    history: createHistory(process.env.VUE_ROUTER_BASE)
+    history: createWebHashHistory(process.env.VUE_ROUTER_BASE)
   })
   setupBeforeEach(Router); // Apply the beforeEach logic to the router
   return Router
